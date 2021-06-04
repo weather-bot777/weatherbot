4c9e2bfd43d1fdf4185e6f568411876b03d9363b
weather-bot777 <80109246+weather-bot777@users.noreply.github.com>
2021-06-04 18:27:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = "YOUR_WEATHER_TOKEN"
-tg_bot_token = "YOUR_TELEGRAM_BOT_TOKEN"
+open_weather_token = "30cdae463392be489c93d941730aed3e"
+tg_bot_token = "1737777316:AAHag-vXMnM4xsEU1wkx3VIKSfrQGtnQqwk"
